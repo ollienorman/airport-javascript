b889f4e31715938bf0516b7b3e8110775b30faa6
Ian Fleming <>
2021-01-12 15:51:36 +0000

    plane takes off

--- spec/features/featureSpec.js
+++ spec/features/featureSpec.js
@@ -1,6 +1,6 @@
 'use strict';
 
-describe('Feature Test:', function(){ 
+describe('Feature Test:', function(){
   var plane;
   var airport;
 
@@ -12,5 +12,11 @@ describe('Feature Test:', function(){
   it('plane can be instructed to land at an airport', function(){
     plane.land(airport);
     expect(airport.planes()).toContain(plane);
+  });
+
+  it('planes can be instructed to takeoff', function(){
+    plane.land(airport)
+    plane.takeoff();
+    expect(airport.planes()).not.toContain(plane);
   });
 });
--- src/airport.js
+++ src/airport.js
@@ -3,11 +3,14 @@
 class Airport {
   constructor() {
     this._hangar = []
-  }
+  };
   planes() {
     return this._hangar;
   };
   clearForLanding(plane) {
     this._hangar.push(plane);
+  };
+  clearForTakeOff(plane) {
+    this._hangar = [];
   };
 };
