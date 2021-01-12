63f88ac6b05ac87575b4926e2a4d48c9822b95bf
ollienorman <oliver.norman1991@gmail.com>
2021-01-12 15:04:28 +0000

    first commit - first feature test passed

---
+++ src/airport.js
@@ -0,0 +1,13 @@
+'use strict';
+
+class Airport {
+  constructor() {
+    this._hangar = []
+  }
+  planes() {
+    return this._hangar;
+  };
+  clearForLanding(plane) {
+    this._hangar.push(plane);
+  };
+};
